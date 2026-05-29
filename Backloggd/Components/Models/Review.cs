5ecd1ccc5a9f6e9f8f2ea97cc376c17ca0cf4ea4
BernhardStoiber <bernhard.stoiber@s.hakmistelbach.ac.at>
2026-05-29 13:21:03 +0200

    Navbar weg, Karten rein

--- Backloggd/Components/Models/Review.cs
+++ Backloggd/Components/Models/Review.cs
@@ -2,8 +2,10 @@
 {
     public class Review
     {
-        public int ReviewId { get; set; }    
-      
+        public int ReviewId { get; set; }
+
+
+        
         public string ReviewText { get; set; }
         public int Rating { get; set; } // 1-100er bewertung
         public User User { get; set; }
