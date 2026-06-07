7c043eee0a240a16a4e46bbcffd7c7ce1a09c2d8
BernhardStoiber <bernhard.stoiber@s.hakmistelbach.ac.at>
2026-06-07 07:28:57 +0200

    Modal fürs hinzufügen/editieren eines Spiels erstellt

--- Backloggd/Models/Spiel.cs
+++ Backloggd/Models/Spiel.cs
@@ -1,21 +1,29 @@
-﻿using System.Reflection.Metadata;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
 
 namespace Backloggd.Models
 {
     public class Spiel
     {
+        
         public int SpielId { get; set; }
+        [Required(ErrorMessage = "Das Spiel braucht einen Namen.")]
         public string Titel { get; set; }
+        [Required(ErrorMessage = "Das Spiel braucht ein Genre.")]
         public string Genre { get; set; }
 
+        [Required(ErrorMessage = "Das Spiel braucht einen Entwickler.")]
         public string Entwickler { get; set; }
-
+        [Required(ErrorMessage = "Das Spiel braucht einen Publisher.")]
         public string Publisher { get; set; }
+        [Required(ErrorMessage = "Das Spiel braucht eine Beschreibung.")]
+
         public string Beschreibung { get; set; }
 
+        [Required(ErrorMessage = "Das Spiel braucht einen Releasezeitpunkt.\nz.B.: 2.Februar 2025")]
         public string Release { get; set; }
-       
-        public decimal DurchschnittsBewertung {  get; set; }
+
+        public decimal DurchschnittsBewertung { get; set; } = 0.00M;
 
        // public Blob Bild { get; set; }
         
--- Backloggd/Services/DataService.cs
+++ Backloggd/Services/DataService.cs
@@ -8,10 +8,11 @@ namespace Backloggd.Services
 {
     public class DataService
     {
-        /*
+        
         // Die Kontext Factory welche die Datenbankverbindung aufbaut
         private readonly IDbContextFactory<AppDBContext> _dbContextFactory;
-        public User loggedInUser { get; private set; }
+
+        public Spiel selectedGame { get; private set; }
 
         // Im Konstruktor wird die ContextFactory übergeben die wir in Program.cs erstellt haben
         // Das passiert automatisch
@@ -20,92 +21,20 @@ namespace Backloggd.Services
             _dbContextFactory = dbContextFactory;
         }
 
-
-        #region User
-
-        public (bool, string) AddUser(User newUser)
+        /*
+        public Spiel GetSpielFromDatabase(int id)
         {
-            try {
+            AppDBContext datenbankVerbindung = _dbContextFactory.CreateDbContext();
 
-                AppDBContext datenbankVerbindung = _dbContextFactory.CreateDbContext();
+           datenbankVerbindung.Spiel.Find().Where(x => x.SpielId == id)
+            datenbankVerbindung.SaveChanges();
 
-                datenbankVerbindung.Add(newUser);
-                datenbankVerbindung.SaveChanges();
-                return (true, string.Empty);
-            }
-            catch (Exception ex)
-            {
-                return (false, ex.Message);
-            }
-        }
-
-        public (bool, string) LoginUser(User loginUser)
-        {
-            try
-            {
-
-                AppDBContext datenbankVerbindung = _dbContextFactory.CreateDbContext();
-
-                string hashedPW = HashPasswort(loginUser.Passwort);
-
-               bool uservorhanden= datenbankVerbindung.User.
-                    Where(u => u.Username == loginUser.Username).
-                    Where(u => u.Passwort == hashedPW).
-                    Any();//Ist genau dieser Datensatz vorhanden?
-
-                if (uservorhanden) //User vorhanden
-                {
-                    this.loggedInUser = loginUser;
-                    return (true, string.Empty);
-                }
-                else
-                {
-                    return (false, "Benutzername oder Passwort falsch");
-                }
-
-
-                
-            }
-            catch (Exception ex)
-            {
-                return (false, ex.Message);
-            }
-        }
-
-        #endregion
-
-        #region Encryption
-
-        private string HashPasswort(string klartext)
-        {
-            byte[] bytes = SHA256.HashData(Encoding.UTF8.GetBytes(klartext));
-            return Convert.ToHexString(bytes).ToLower();
-        }
-
-        #endregion
-
-
-
-        #region Nachrichten
-
-        public List<Nachrichten> GetNachrichten()
-        {
-            List<Nachrichten> messages= new List<Nachrichten>();
-            try
-            {
-
-                AppDBContext datenbankVerbindung = _dbContextFactory.CreateDbContext();
-                messages=datenbankVerbindung.Nachrichten.OrderByDescending(n=>n.Created).Include(n=>n.User).ToList();
-            }
-            catch (Exception ex) 
-            { 
             
-            }
-            return messages;
-        }
+            
 
-        #endregion
-         */
+        }
+        */
+       
     }
 
 
